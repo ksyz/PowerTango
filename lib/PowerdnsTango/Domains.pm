7470b88188632fba42f15b99ce6bc5616e2b3d69
Michal Ingeli <mi@v3.sk>
2013-04-24 12:50:05 +0200

    Domain add handled by Ajax plugin. Simplified most of the process.

--- lib/PowerdnsTango/Domains.pm
+++ lib/PowerdnsTango/Domains.pm
@@ -240,6 +240,115 @@ post '/domains/add' => sub
 	return redirect '/domains';
 };
 
+ajax '/domains/add' => sub {
+	my $domain = params->{add_domain_name} || 0;
+	my $type = params->{add_domain_type} || 0;
+	my $master = params->{add_master_addr};
+	my $template_id = params->{add_domain_template} || 0;
+	my $user_type = session 'user_type';
+	my $user_id = session 'user_id';
+	my $dt = DateTime->now;
+	my ($year,$month,$day) = split(/-/, $dt->ymd('-'));
+	my $success = 0;
+	my $sth;
+
+	$domain =~ s/\s//g;
+	$master =~ s/\s//g 
+		if (defined $master);
+
+	return { stat => 'fail', message => "Invalid domain name"}
+		unless is_domain($domain);
+
+	unless ($user_type eq 'admin') {
+		my $user_domain_limit = database->quick_select('users_tango', { id => $user_id });
+		$sth = database->prepare("SELECT COUNT(id) AS count FROM domains_acl_tango WHERE user_id = ?");
+		$sth->execute($user_id);
+		my $owned_domains = $sth->fetchrow_hashref;
+
+		return {
+			stat => 'fail',
+			message => "You have reached your domain limit of $user_domain_limit->{domain_limit}"
+		} if ($owned_domains->{count} >= $user_domain_limit->{domain_limit});
+	}
+
+	return { stat => 'fail', message => "Domain $domain already exists"}
+		if database->quick_lookup('domains', {'name' => $domain}, 'id');
+
+	my $domain_id = undef; # ID of new domain
+
+	if ($type eq 'NATIVE' || $type eq 'MASTER') {
+		$success = database->quick_insert('domains', 
+			{ name => $domain, type => $type, notified_serial => ($year . $month . $day . 0 . 1) });
+	}
+	elsif ($type eq 'SLAVE') {
+		# checking for valid list of masters (check also rewrites value)
+		return { stat => 'fail', 
+			message => "A vaild master address or list of ".
+				"comma separated master addresses must be provided" 
+		} unless check_valid_masters(\$master);
+
+		$success = database->quick_insert('domains', 
+			{ name => $domain, type => $type, master => $master, 
+			notified_serial => ($year . $month . $day . 0 . 1) });
+	}
+
+	if ($success) {
+		$domain_id = database->last_insert_id(undef, undef, 'domains', 'id');
+		return { stat => 'fail', message => 'last_insert_id failed' }
+			unless defined $domain_id;
+		database->quick_insert('domains_acl_tango', { user_id => $user_id, domain_id => $domain_id });
+	}
+	else {
+		return { stat => 'fail', message => 'Domain insert query failed' };
+	}
+
+	if ($type ne 'SLAVE' && $success) {
+		# Applying RR from template
+		if ($template_id != 0) {
+			my $templates_records = database->prepare('SELECT * FROM templates_records_tango WHERE template_id = ?');
+			$templates_records->execute($template_id);
+
+			while (my $template_row = $templates_records->fetchrow_hashref) {
+				$template_row->{name} =~ s/\%(\s)?(zone|domain|host)(\s)?\%/$domain/i;
+				$template_row->{name} =~ s/\%(\s)?(.+?)(\s)?\%//i;
+				$template_row->{content} =~ s/\%(\s)?(zone|domain|host)(\s)?\%/$domain/i;
+				$template_row->{content} =~ s/\%(\s)?(.+?)(\s)?\%//i;
+				
+				database->quick_insert('records', { 
+					domain_id => $domain_id, name => $template_row->{name}, 
+					type => $template_row->{type}, content => $template_row->{content}, 
+					ttl => $template_row->{ttl}, prio => $template_row->{prio}, 
+					change_date => ($year . $month . $day . 0 . 1)
+				});
+			}
+		}
+
+		# Applying defaul SOA
+		my $default_soa = database->quick_select('admin_default_soa_tango', {});
+
+		if (defined $default_soa->{name_server} && defined $default_soa->{contact} 
+			&& defined $default_soa->{refresh} && defined $default_soa->{retry} 
+			&& defined $default_soa->{expire} && defined $default_soa->{minimum} 
+			&& defined $default_soa->{ttl}) {
+			my $content = ($default_soa->{name_server} . " " . $default_soa->{contact} . " " . $year . $month . $day . 0 . 1 . " " . $default_soa->{refresh} . " " . $default_soa->{retry} . " " . $default_soa->{expire} . " " . $default_soa->{minimum});
+			
+			database->quick_insert('records', { 
+				domain_id => $domain_id, name => $domain, type => 'SOA', 
+				content => $content, ttl => $default_soa->{ttl}, 
+				change_date => ($year . $month . $day . 0 . 1)
+			});
+		}
+	}
+
+	return {
+		stat => 'ok',
+		id => $domain_id,
+		message => "Domain $domain added",
+		name => $domain,
+		type => $type,
+		master => $master,
+	};
+};
 
 post '/domains/add/bulk' => sub
 {
