7c85a0d09f2c75847259adb49001f76e1f4db1f2
Michal Ingeli <mi@v3.sk>
2013-04-24 15:15:07 +0200

    cleaning up PT::Acl::user_acl, but still subject to refactor, as swapped values for true/false are not intuitive

--- lib/PowerdnsTango/Acl.pm
+++ lib/PowerdnsTango/Acl.pm
@@ -2,46 +2,48 @@ package PowerdnsTango::Acl;
 use Dancer ':syntax';
 use Dancer::Plugin::Database;
 use Dancer::Session::Storable;
+use Scalar::Util qw(looks_like_number);
+
 use base "Exporter";
 
 our @EXPORT = qw(user_acl);
 our $VERSION = '0.1';
 
 
-sub user_acl
-{
+# To confuse, 
+# 1 => no access
+# 0 => access granted
+sub user_acl {
 	my ($obj_id, $obj_type) = @_;
 	my $user_type = session 'user_type';
 	my $user_id = session 'user_id';
 	my $acl;
 	my $check_acl;
 
+	return 0 
+		if ($user_type eq 'admin');
 
-	return 0 if ($user_type eq 'admin');
+	return 1
+		if (not defined $obj_id or !looks_like_number($obj_id) or not defined $obj_type);
 
-
-	if ((defined $obj_id && $obj_id =~ m/^(\d)+$/x) && (defined $obj_type && $obj_type eq 'domain'))
-	{
-		$acl = database->prepare("select count(id) as count from domains_acl_tango where domain_id = ? and user_id = ?");
-		$acl->execute($obj_id, $user_id);
-		$check_acl = $acl->fetchrow_hashref;
+	if ($obj_type eq 'domain') {
+		$acl = database->prepare("SELECT COUNT(id) AS count FROM domains_acl_tango WHERE domain_id = ? AND user_id = ?");
 	}
-	elsif ((defined $obj_id && $obj_id =~ m/^(\d)+$/x) && (defined $obj_type && $obj_type eq 'template'))
-	{
-		$acl = database->prepare("select count(id) as count from templates_acl_tango where template_id = ? and user_id = ?");
-		$acl->execute($obj_id, $user_id);
-		$check_acl = $acl->fetchrow_hashref;
+	elsif ($obj_type eq 'template') {
+		$acl = database->prepare("SELECT COUNT(id) AS count FROM templates_acl_tango WHERE template_id = ? AND user_id = ?");
+	}
+	else {
+		# this is more severe, as we probably did som type there
+		die 'Invalid ACL object type';
 	}
 
+	$acl->execute($obj_id, $user_id);
+	$check_acl = $acl->fetchrow_hashref;
 
-	if ($check_acl->{count} == 0)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
+	return 0 
+		if $check_acl->{count} > 0;
+
+	return 1;
 };
 
-
+1;
